392f1a1de1164877e897e0fc2eb19a1ae6c0cf91
Diana1505rojas <diana.rojas2@udea.edu.co>
2024-03-03 22:40:18 -0500

    codigo arreeglado

--- clasev3.py
+++ clasev3.py
@@ -33,11 +33,15 @@ class Sistema:
         for p in self.__lista_pacientes:
             if cedula == p.verCedula():
                 return True 
-        return False
+        return None
         
     def ingresarPaciente(self,pac):
-        self.__lista_pacientes.append(pac)
-        return True
+         # Primero verifica que el paciente no exista antes de añadirlo
+        if self.verificarPaciente(pac.verCedula) == True:
+            return None
+        else:
+            self.__lista_pacientes.append(pac)
+            return True
     
     def verDatosPaciente(self, c):
         if self.verificarPaciente(c) == False:
@@ -50,15 +54,40 @@ class Sistema:
     def verNumeroPacientes(self):
         print("En el sistema hay: " + str(len(self.__lista_pacientes)) + " pacientes") 
 
+    # Creamos un metodo dentro de la clase para poder acceder a la lista de pacientes
+        def verLista(self):
+            lista = self.__lista_pacientes
+            return lista
+        
 def main():
     sis = Sistema() 
-    #probemos lo que llevamos programado
+        # lineas usadas para la prueba de la busqueda por nombre
+    # pena = Paciente()
+    # pena.asignarNombre("Juan Esteban Pena ")
+    # pena.asignarCedula(1085902252)
+    # pena.asignarGenero("M")
+    # pena.asignarServicio("s")
+    # sis.ingresarPaciente(pena)
+
+    # pena = Paciente()
+    # pena.asignarNombre("Juan Pena ")
+    # pena.asignarCedula(1085902257)
+    # pena.asignarGenero("M")
+    # pena.asignarServicio("s")
+    # sis.ingresarPaciente(pena)
+    
+    # tban = Paciente()
+    # tban.asignarNombre("Esteban Daniel Pena Rojas")
+    # tban.asignarCedula(1085902256)
+    # tban.asignarGenero("M")
+    # tban.asignarServicio("s")
+    # sis.ingresarPaciente(tban
     while True:
         #TAREA HACER EL MENU
         opcion = int(input("\nIngrese \n0 para salir, \n1 para ingresar nuevo paciente, \n2 ver Paciente\n\t--> ")) 
         
         if opcion == 1:
-            #ingreso pacientes
+            opcion = int(input("\nDigite \n0 para salir, \n1 para ingresar nuevo paciente, \n2 para ver un paciente\n\t--> "))
             print("A continuacion se solicitaran los datos ...") 
             #1. Se solicitan los datos
             cedula = int(input("Ingrese la cedula: ")) 
@@ -78,25 +107,75 @@ def main():
                     print("Paciente ingresado") 
                 else:
                     print("No ingresado") 
+
+
+
+
+        #ver pacientes 
         elif opcion == 2:
-            #1. solicito la cedula que quiero buscar
-            c = int(input("Ingrese la cedula a buscar: ")) 
-            #le pido al sistema que me devuelva en la variable p al paciente que tenga
-            #la cedula c en la lista
-            p = sis.verDatosPaciente(c) 
-            #2. si encuentro al paciente imprimo los datos
-            if p != None:
-                print("Nombre: " + p.verNombre()) 
-                print("Cedula: " + str(p.verCedula())) 
-                print("Genero: " + p.verGenero()) 
-                print("Servicio: " + p.verServicio()) 
-            else:
-                print("No existe un paciente con esa cedula") 
+                        # creamos un bucle para la selección de opciones del sub-menú ver paciente
+            while True:
+                opcion = int(input("\nSeleccione el tipo de busqueda \n0 Para cancelar busqueda, \n1 Para busqueda por cedula, \n2 Para busqueda por nombre\n\t--> "))
+                    # Busqueda por cedula
+                if opcion==1:
+                    #1. solicito la cedula que quiero buscar
+                    c = int(input("Ingrese la cedula a buscar: ")) 
+                    #le pido al sistema que me devuelva en la variable p al paciente que tenga
+                    #la cedula c en la lista
+                    p = sis.verDatosPaciente(c) 
+                    #2. si encuentro al paciente imprimo los datos
+                    if p != None:
+                        print("Nombre: " + p.verNombre()) 
+                        print("Cedula: " + str(p.verCedula())) 
+                        print("Genero: " + p.verGenero()) 
+                        print("Servicio: " + p.verServicio()) 
+                    else:
+                        print("No existe un paciente con esa cedula") 
+                # Busqueda por nombre
+                elif opcion==2:
+
+
+                    
+                    nombre = str(input("Ingrese el nombre del paciente: ")) #1. solicito el nombre que quiero buscar
+                    nombres_separados = nombre_para_busqueda(nombre)        #Creo una lista iterable de los nombres del paciente
+                    lista = sis.verLista()                                  #Asigno a "lista" la lista de pacientes del Sistema
+                    lista_coincidencias = []                                #Creo una lista vacia para guardar las coincidencias del nombre
+
+                    for nombre_I in nombres_separados:                                  #itearamos sobre cada nombre ingresado
+                        for objeto in lista:                                            #iteramos para cada nombre sobre los objetos de la lista de pacientes
+                            nombre_C_lista = nombre_para_busqueda(objeto.verNombre())   #convertimos el nombre de cada paciente en una lista iterable "nombre_C_lista"
+                            for nombre_C in nombre_C_lista:                             #iteramos cada nombres de la lista de pacientes 
+                                if nombre_I == nombre_C:                                #comparamos para identificar una coincidencia en alguno de los nombres o apellidos
+                                    if objeto not in lista_coincidencias:               #verificamos que no se haya encontrado aún
+                                        lista_coincidencias.append(objeto)              #en caso de ser una coincidencia nueva, se añade a la lista
+                                        break                                           #break para dejar de iterar sobre los nombres del paciente tras añadirlo y pasar al siguiente
+
+                    print(f"Se encontró(aron) {len(lista_coincidencias)} coincidencia(s)")   #se informa la cantidad de pacientes con coincidencia en el nombre consultado
+
+                    for p in lista_coincidencias:                                       #se muestra la informacion de cada paciente encontradod
+
+                        print("Nombre: " + p.verNombre()) 
+                        print("Cedula: " + str(p.verCedula())) 
+                        print("Genero: " + p.verGenero()) 
+                        print("Servicio: " + p.verServicio() +"\n") 
+
+
+                elif opcion!=0:
+                    continue
+                else:
+                    break
+
         elif opcion !=0:
             continue 
         else:
             break 
 
+def nombre_para_busqueda(nombre):       #creamos una funcion nueva para transformar un string a una lista separando 
+    nombre_normal = nombre.lower()      #normalizamos la cadena a minusculas
+    lista = nombre_normal.split()       #separamos la cadena por espacios y se asigna a una lista
+    return lista                        #se retorna la lista
+
+
 #aca el python descubre cual es la funcion principal
-if __name__ == "__main__":
-    main() 
+if _name_ == "main":
+    main()
